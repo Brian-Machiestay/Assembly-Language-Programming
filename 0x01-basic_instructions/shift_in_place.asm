3a3218355cd53ee080579a9b1e24fac2ecdab158
Brian-Machiestay <brianmachiestay@gmail.com>
2024-08-15 14:45:01 +0000

    left shift and in place

---
+++ 0x01-basic_instructions/shift_in_place.asm
@@ -0,0 +1,7 @@
+# program to shift in place
+            .text
+            .globl main
+
+main:
+            ori   $8, $0, 4576
+            sll   $9, $8, 2
---
+++ 0x01-basic_instructions/shift_left.asm
@@ -0,0 +1,9 @@
+# program to logical shift left a pattern
+            .text
+            .globl main
+
+main:
+            ori   $8, $0, 0x6F
+            sll $9, $8, 2
+
+## End of file
